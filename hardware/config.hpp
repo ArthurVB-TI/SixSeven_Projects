5201c3e8132a5bff0938f545e14c52f8fc8389a8
Vinícius Coelho <viniciusraposo@gmail.com>
2026-07-03 11:45:05 -0300

    ops

--- hardware/config.hpp
+++ hardware/config.hpp
@@ -1,7 +1,45 @@
-#include "Dados.hpp"
-#include "Hooks.hpp"
-#include "hardware.hpp"
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
 
-// Constantes
-const int PIN_led = 0;
-const int PIN_interruptor = 0;
+#include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <BluetoothSerial.h>
+
+#define WIFI_SSID "SUA_REDE"
+#define WIFI_PASSWORD "SUA_SENHA"
+
+#define SERVER_HOST "192.168.0.10"
+#define SERVER_PORT 3000
+#define SERVER_PATH "/data"
+#define SERVER_CONFIG_PATH "/config"
+
+#define BT_NAME "ESP32_Estabilizador"
+
+#define CONNECTION_ID 1
+
+#define PIN_POT_RECEIVED 36
+#define PIN_POT_BASE 39
+#define PIN_BUTTON 4
+#define PIN_BUZZER 13
+#define PIN_OUTPUT 25
+
+#define PWM_CH_OUTPUT 0
+#define PWM_CH_BUZZER 1
+#define PWM_FREQ 5000
+#define PWM_RES 8
+
+#define ADC_MAX 4095
+#define PWM_MAX 255
+
+#define SEND_INTERVAL 2000
+#define READ_INTERVAL 100
+#define RECONNECT_INTERVAL 5000
+#define DEBOUNCE_INTERVAL 200
+
+#define WINDOW_SIZE 10
+#define STABLE_THRESHOLD 150
+#define ALERT_TONE 1200
+#define BEEP_DURATION 60
+
+#endif
--- hardware/hardware.hpp
+++ hardware/hardware.hpp
@@ -1,13 +1,50 @@
-class Hardware{
+#ifndef HARDWARE_HPP
+#define HARDWARE_HPP
+
+#include "config.hpp"
+#include "connection.hpp"
+#include "hooks.hpp"
+#include "data.hpp"
+
+class Hardware {
+    private:
+        DataRepository data;
+        Connection conn;
+        Hooks hooks;
+
+        bool powered;
+        bool lastButton;
+        unsigned long lastDebounce;
+        unsigned long beepEnd;
+
     public:
-        // Metodos de getter
-        
-        
-        // Metodos de Manipulação de dados
+        Hardware();
 
+        void begin();
 
-        // Metodos de print no serial
+        int readReceivedEnergy();
+        int readBaseEnergy();
+        bool readButton();
 
+        void computeAll();
 
-        // Metodos de Setter
+        void driveOutput();
+        void alert();
+        void stopAlert();
+
+        void showSerial();
+
+        void collector();
+        void manipulator();
+        void controller();
+        void printer();
+
+        bool push();
+        bool pull();
+
+        bool isPowered();
+        void keepConnection();
+        int getTimer();
 };
+
+#endif
